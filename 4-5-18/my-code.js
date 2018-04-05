d1d73ef1954ca10ead8ac3ef48d6a4d706276cbd
Harry Wang <harryjwang@gmail.com>
2018-04-05 10:37:54 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,13 @@
+blist = ["end of the semster", "no research", "Disen gets better"];
+console.log(blist.length);
+blist.push("get a beer");
+
+console.log(blist.length);
+console.log(blist);
+
+/* for loop
+blablba
+*/
+for (i=0; i< 4; i++) {
+    console.log(blist[i]);
+}
